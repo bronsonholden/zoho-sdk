96dda7a144fb28a73db97e74ba537e4baf32d022
Paul Holden <paul@holdensoftware.com>
2020-01-03 11:24:46 -0800

    Workspace exists test for non-existing

--- spec/analytics/workspace_spec.rb
+++ spec/analytics/workspace_spec.rb
@@ -1,9 +1,19 @@
 RSpec.describe Zoho::Analytics::Workspace do
-  let(:workspace) { client.workspace("Workspace") }
+  let(:name) { "Missing Workspace" }
+  let(:workspace) { client.workspace(name) }
 
   describe "#exists?" do
-    it "returns true for existing workspace" do
-      expect(workspace.exists?).to eq(true)
+    context "with existing" do
+      let(:name) { "Workspace" }
+      it "exists" do
+        expect(workspace.exists?).to eq(true)
+      end
+    end
+
+    context "with missing" do
+      it "does not exist" do
+        expect(workspace.exists?).to eq(false)
+      end
     end
   end
 end
